660e42324a1095bd619096b01d721fccb015768b
Jack Cuthbert <jckcthbrt@gmail.com>
2021-11-19 18:52:46 +1100

    fix: ensure css purged from layouts directory
    Additionally adds sharp as production dependency based on CLI
    recommendation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ const typography = require('@tailwindcss/typography')
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
