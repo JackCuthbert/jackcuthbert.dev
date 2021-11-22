ecfc102d742de6932ee7cca14e15ab5f4fc86cf2
Jack Cuthbert <jckcthbrt@gmail.com>
2021-11-22 19:38:46 +1100

    feat: use next/script for clicky.js

--- src/components/ClickyScript.tsx
+++ src/components/ClickyScript.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
+import Script from 'next/script'
 
 export function ClickyScript(): JSX.Element {
   return (
-    <>
-      <script async src="//static.getclicky.com/101258162.js" />
-      <noscript>
-        <p>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            alt="Clicky"
-            width="1"
-            height="1"
-            src="//in.getclicky.com/101258162ns.gif"
-          />
-        </p>
-      </noscript>
-    </>
+    <Script
+      strategy="afterInteractive"
+      src="https://static.getclicky.com/101258162.js"
+    />
   )
 }
