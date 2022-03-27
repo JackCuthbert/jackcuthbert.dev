b8e93e0f483da16cb475dfdb8d07909e77cd4ff0
Jack Cuthbert <jckcthbrt@gmail.com>
2022-03-28 07:36:06 +1100

    fix: clicky pls

--- src/components/ClickyScript.tsx
+++ src/components/ClickyScript.tsx
@@ -3,9 +3,18 @@ import Script from 'next/script'
 
 export function ClickyScript(): JSX.Element {
   return (
-    <Script
-      strategy="afterInteractive"
-      src="https://static.getclicky.com/101258162.js"
-    />
+    <>
+      <script async src="//static.getclicky.com/101258162.js" />
+      <noscript>
+        <p>
+          <img
+            alt="Clicky"
+            width="1"
+            height="1"
+            src="//in.getclicky.com/101258162ns.gif"
+          />
+        </p>
+      </noscript>
+    </>
   )
 }
